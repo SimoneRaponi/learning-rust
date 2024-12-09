77a1c29728b081c8fe81e077f0cd67f60ae5163e
simone <s.raponi.93@gmail.com>
2024-12-09 11:33:50 +0100

    Add struct to manage tasks

--- advent_of_code_2024/challenge-09/src/main.rs
+++ advent_of_code_2024/challenge-09/src/main.rs
@@ -1,21 +1,26 @@
 use std::fs::File;
 use std::io::{self, Read};
 
+struct Processor {
+    row_input: String,
+}
+
 fn main() -> io::Result<()> {
 
     let file_path = "test.txt";
 
+    let mut processor = Processor{
+        row_input: String::new()
+    };
+
     // Open the file
     let mut file = File::open(file_path)?;
 
-    // Create a String to store the file content
-    let mut content = String::new();
-
     // Read the file content into the String
-    file.read_to_string(&mut content)?;
+    file.read_to_string(&mut processor.row_input)?;
 
     // Print the content (optional)
-    println!("File content:\n{}", content);
+    println!("File content:\n{}", processor.row_input);
 
     Ok(())
 }
